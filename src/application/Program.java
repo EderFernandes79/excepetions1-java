32ca19b0d59127d0c39b6574995389f772b76a43
Eder Fernandes <ederfernandes124751@gmail.com>
2020-08-12 09:15:09 -0400

    Very Bad Solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,21 @@
 
 
-// METODO BOM = usando tratamento de exceções 
-// CRIANDO EXCEÇÕES
-// USANDO O METODO = Exception
+
+/*
+Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data
+de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
+dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+novamente a reserva com os dados atualizados. O programa não deve aceitar dados
+inválidos para a reserva, conforme as seguintes regras:
+- Alterações de reserva só podem ocorrer para datas futuras
+- A data de saída deve ser maior que a data de entrada
+*/
+
+
+
+
+// => SOLUÇÃO MUITO RUIM : lógica de validação no programa principal
+// => Lógica de validação não delegada à reserva
 
 
 
@@ -12,60 +25,60 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import model.entities.Reservation;
-import model.exceptions.Domain_Exception;
 
 public class Program {
     
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws ParseException {
         
     
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
         
-        try{ 
-
-            System.out.print("Room Number: ");
-            int number = sc.nextInt();
-            System.out.print("Check-in Date (dd/MM/yyyy): ");
-            Date checkin = sdf.parse(sc.next()); //converte o texto pro formato date
-            //sdf.parse(sc.next())= pode lançar um exceção no metodo, adicionar o throws ParseException
-            System.out.print("Check-out Date (dd/MM/yyyy): ");
-            Date checkout = sdf.parse(sc.next());
+        System.out.print("Room Number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in Date (dd/MM/yyyy): ");
+        Date checkin = sdf.parse(sc.next()); //converte o texto pro formato date
+        //sdf.parse(sc.next())=pode lançar um exceção no metodo, adiciona-se o throws ParseException
+        System.out.print("Check-out Date (dd/MM/yyyy): ");
+        Date checkout = sdf.parse(sc.next());
+        
+        
+        if ( ! checkout.after(checkin)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else {
 
             Reservation reservation = new Reservation(number, checkin, checkout);
             System.out.println("Reservation: " + reservation);
-
-            System.out.println("\n");
-
+            
+            System.out.println();
+            
             System.out.println("Enter data to update the reservation:");
             System.out.print("Check-in Date (dd/MM/yyyy): ");
-            checkin = sdf.parse(sc.next()); 
+            checkin = sdf.parse(sc.next()); //converte o texto pro formato date
+            //sdf.parse(sc.next())=pode lançar um exceção no metodo, adiciona-se o throws ParseException
             System.out.print("Check-out Date (dd/MM/yyyy): ");
             checkout = sdf.parse(sc.next());
+            
+            //atualização =  versão muito ruim, logica de validação era para estar na classe Reservation
 
-
-            // metodo de atualização de reserva 
-            reservation.updateDates(checkin, checkout);
-
-            System.out.println("Reservation" + reservation);
-
+            Date now = new Date();
+            if( checkin.before(now) || checkout.before(now) ){
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
             }
-        
-            //tratando o ParseException
-            catch ( ParseException e ){
-                System.out.println("Invalid date format");
+            else if( ! checkout.after(checkin)){
+               System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }
+            else{
+                reservation.updateDates(checkin, checkout);
+                System.out.println("Reservation" + reservation);
             }
             
-            //nao deixa o programa "quebrar"
-            catch ( Domain_Exception e ){
-                System.out.println("Error in Reservation: " + e.getMessage() );
-            }
-            //herança e upcasting
-            catch ( RuntimeException e ) {
-                System.out.println("Unexpected error");
-            }
-              
+        }
+        
+        
+
         sc.close();
     }//fim main
     
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,23 +1,4 @@
 
-/*
-
-Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data
-de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
-dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
-novamente a reserva com os dados atualizados. O programa não deve aceitar dados
-inválidos para a reserva, conforme as seguintes regras:
-- Alterações de reserva só podem ocorrer para datas futuras
-- A data de saída deve ser maior que a data de entrada
-
-*/
-
-
-
-// METODO BOM = usando tratamento de exceções 
-// CRIANDO EXCEÇÕES
-
-// USANDO O METODO EXCEPTION =  no Pacote model.exceptions na classe Domain_Exceptions
-
 
 
 package model.entities;
@@ -25,7 +6,6 @@ package model.entities;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import model.exceptions.Domain_Exception;
 
 public class Reservation {
     
@@ -39,17 +19,13 @@ public class Reservation {
     
     //construtores
     public Reservation(Integer roomNumber, Date checkin, Date checkout) {
-    // tratando exceções
-       if ( ! checkout.after(checkin) ) {
-               throw new Domain_Exception ("Check-out date must be after check-in date");
-            }
-            this.roomNumber = roomNumber;
-            this.checkin = checkin;
-            this.checkout = checkout;
-        }
-    
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
     
     //modificadores 
+
     public Integer getRoomNumber() {
         return roomNumber;
     }
@@ -62,27 +38,18 @@ public class Reservation {
     public Date getCheckout() {
         return checkout;
     }
-    
+
     //metodo de duração de estadia em dias
     public long duration(){
       long diff = checkout.getTime() - checkin.getTime(); // diferença entre as datas em milesegundos
       return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // converte milesegundos para dias 
     }
-
+ 
     //metodo para atualizar as datas 
-    public void updateDates (Date checkin, Date checkout) {
-        
-        Date now = new Date();
-        if (checkin.before(now) || checkout.before(now) ) {
-           throw new Domain_Exception ("Reservation dates for update must be future dates");
-        }
-        if ( ! checkout.after(checkin) ) {
-           throw new Domain_Exception ("Check-out date must be after check-in date");
-        }
+    public void updateDates (Date checkin, Date checkout){
         this.checkin = checkin;
         this.checkout = checkout;
-
-    } 
+    }
     
     //toString
     @Override
